86ad2edeeed244953a127a31d93622d2e3aa4527
Johaib Ansari <142145170+zohai-ibb@users.noreply.github.com>
2026-08-25 22:19:00 +0530

    Time: 0 ms (100%), Space: 42.8 MB (91.13%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> list = new ArrayList<>();
+
+        int rowStart = 0;
+        int rowEnd = matrix.length - 1;
+
+        int colStart = 0;
+        int colEnd = matrix[0].length - 1;
+
+        while (rowStart <= rowEnd && colStart <= colEnd) {
+
+            // Left -> Right
+            for (int i = colStart; i <= colEnd; i++) {
+                list.add(matrix[rowStart][i]);
+            }
+            rowStart++;
+
+            // Top -> Bottom
+            for (int i = rowStart; i <= rowEnd; i++) {
+                list.add(matrix[i][colEnd]);
+            }
+            colEnd--;
+
+            // Right -> Left
+            if (rowStart <= rowEnd) {
+                for (int i = colEnd; i >= colStart; i--) {
+                    list.add(matrix[rowEnd][i]);
+                }
+                rowEnd--;
+            }
+
+            // Bottom -> Top
+            if (colStart <= colEnd) {
+                for (int i = rowEnd; i >= rowStart; i--) {
+                    list.add(matrix[i][colStart]);
+                }
+                colStart++;
+            }
+        }
+
+        return list;
+    }
+}
