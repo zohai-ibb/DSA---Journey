0535099b9e543eceecdeba3812801e3e3f0d8850
Johaib Ansari <142145170+zohai-ibb@users.noreply.github.com>
2026-09-16 13:23:49 +0530

    Time: 0 ms (100%), Space: 43.1 MB (50.64%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,34 +1,31 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
-        int low = 0;
-        int high = 0;
-        int ans = 0;
-        for(int i=0; i<nums.length; i++){
-            low = Math.max(low, nums[i]);
-            high += nums[i];
+        int low = Integer.MIN_VALUE, high = 0;
+        for (int num : nums) {
+            low = Math.max(low, num);
+            high = high + num;
         }
-        while(low <= high) {
+        
+        while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (isPossible(nums, mid, k)) {
-                ans = mid;
-                high = mid - 1;
-            }else{
-                low = mid + 1;
-            }
+            if (check(nums, mid, k)) high = mid - 1;
+            else low = mid + 1;
         }
-        return ans;
+        
+        return low;
     }
-
-    static boolean isPossible(int[] arr, int mid, int m){
-        int subArray = 1;
-        int sum = 0;
-        for(int i=0; i<arr.length; i++){
-            sum += arr[i];
-            if(sum > mid){
-                subArray++;
-                sum = arr[i];
-            }
+    private boolean check(int[] nums,int mid,int k) {
+        int subarrays = 1, sum = 0;
+        
+        for (int num : nums) {
+            if (sum + num > mid) {
+                subarrays++;
+                sum = num;
+            } else sum += num;
+            
+            if (subarrays > k) return false;
         }
-        return subArray <= m;
+        
+        return true;
     }
 }
