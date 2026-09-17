3a2b53192102b59dc74fa29057d464045861d43a
Johaib Ansari <142145170+zohai-ibb@users.noreply.github.com>
2026-09-17 14:23:40 +0530

    Time: 6 ms (88.25%), Space: 43.9 MB (41.65%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,30 +1,31 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-
-    if (s.length() != t.length()) {
-        return false;
-    }
-
-    HashMap<Character, Character> mapST = new HashMap<>();
-    HashMap<Character, Character> mapTS = new HashMap<>();
-
-    for (int i = 0; i < s.length(); i++) {
-
-        char a = s.charAt(i);
-        char b = t.charAt(i);
-
-        if (mapST.containsKey(a) && mapST.get(a) != b) {
+        // Create arrays to store the index of characters in both strings
+        int[] indexS = new int[200]; // Stores index of characters in string s
+        int[] indexT = new int[200]; // Stores index of characters in string t
+        
+        // Get the length of both strings
+        int len = s.length();
+        
+        // If the lengths of the two strings are different, they can't be isomorphic
+        if(len != t.length()) {
             return false;
         }
-
-        if (mapTS.containsKey(b) && mapTS.get(b) != a) {
-            return false;
+        
+        // Iterate through each character of the strings
+        for(int i = 0; i < len; i++) {
+            // Check if the index of the current character in string s
+            // is different from the index of the corresponding character in string t
+            if(indexS[s.charAt(i)] != indexT[t.charAt(i)]) {
+                return false; // If different, strings are not isomorphic
+            }
+            
+            // Update the indices of characters in both strings
+            indexS[s.charAt(i)] = i + 1; // updating index of current character
+            indexT[t.charAt(i)] = i + 1; // updating index of current character
         }
-
-        mapST.put(a, b);
-        mapTS.put(b, a);
+        
+        // If the loop completes without returning false, strings are isomorphic
+        return true;
     }
-
-    return true;
-}
 }
