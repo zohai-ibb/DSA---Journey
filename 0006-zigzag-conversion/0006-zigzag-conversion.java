ccc66b95f5c45a86181ae64b8564c1f9a2e76b85
Johaib Ansari <142145170+zohai-ibb@users.noreply.github.com>
2026-09-21 11:49:56 +0530

    Time: 5 ms (51.24%), Space: 46.7 MB (36.03%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,31 +1,34 @@
 class Solution {
     public String convert(String str, int row) {
-        String s = "";
-        String ans[] = new String[row];
+        StringBuilder s = new StringBuilder();
+        StringBuilder ans[] = new StringBuilder[row];
 
         for (int i = 0; i < row; i++) {
-            ans[i] = "";
+            ans[i] = new StringBuilder();
         }
 
         int i = 0;
-        while(i < str.length()){
 
+        while (i < str.length()) {
+
+            // Going down
             for (int j = 0; j < ans.length && i < str.length(); j++) {
-                ans[j]  += str.charAt(i) + "";
+                ans[j].append(str.charAt(i));
                 i++;
             }
 
+            // Going up
             for (int j = row - 2; j > 0 && i < str.length(); j--) {
-                ans[j] += str.charAt(i);
+                ans[j].append(str.charAt(i));
                 i++;
             }
-
         }
 
+        // Combine all rows
         for (int j = 0; j < ans.length; j++) {
-            s += ans[j];
+            s.append(ans[j]);
         }
 
-        return s;
+        return s.toString();
     }
 }
