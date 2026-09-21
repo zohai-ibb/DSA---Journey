6a8ecec68053b600a738f65321fde9ecd4929200
Johaib Ansari <142145170+zohai-ibb@users.noreply.github.com>
2026-09-21 22:13:38 +0530

    Time: 269 ms (23.38%), Space: 43.8 MB (48.92%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int repeatedStringMatch(String a, String b) {
+        StringBuilder str = new StringBuilder();
+
+        int count = 0;
+
+        while (str.length() < b.length()) {
+            str.append(a);
+            count++;
+        }
+
+        if (str.toString().contains(b)) {
+            return count;
+        }
+
+        str.append(a);
+        count++;
+
+        if (str.toString().contains(b)) {
+            return count;
+        }
+
+        return -1;
+    }
+}
