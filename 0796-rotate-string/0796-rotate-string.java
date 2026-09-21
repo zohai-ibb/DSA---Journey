5e7b9842d7e0c5ee109322799d410d701a8a0290
Johaib Ansari <142145170+zohai-ibb@users.noreply.github.com>
2026-09-22 01:23:00 +0530

    Time: 1 ms (98.37%), Space: 43.3 MB (82.26%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,30 +1,30 @@
 class Solution {
     public boolean rotateString(String s, String g) {
 
-    if (s.length() != g.length()) {
-        return false;
-    }
+        if (s.length() != g.length()) {
+            return false;
+        }
 
-    for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
 
-        if (s.charAt(0) == g.charAt(i)) {
+            if (s.charAt(0) == g.charAt(i)) {
 
-            boolean check = true;
+                boolean check = true;
 
-            for (int j = 0; j < s.length(); j++) {
+                for (int j = 0; j < s.length(); j++) {
 
-                if (s.charAt(j) != g.charAt((i + j) % g.length())) {
-                    check = false;
-                    break;
+                    if (s.charAt(j) != g.charAt((i + j) % g.length())) {
+                        check = false;
+                        break;
+                    }
+                }
+
+                if (check) {
+                    return true;
                 }
             }
-
-            if (check) {
-                return true;
-            }
         }
-    }
 
-    return false;
-}
+        return false;
+    }
 }
